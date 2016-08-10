8c8dc38efc8a82d002ee7a252ef5adc4547a5a07
Christopher Jakob <christopher.m.jakob@gmail.com>
2016-08-10 00:54:16 -0500

    added more pseudo code to classes. created additional classes. created sequence diagram for general group poster (not on git yet)

---
+++ source/PG.py
@@ -0,0 +1,22 @@
+from genericImportClass import *
+
+class PG:
+
+    def __init__(self, payload):
+        self.payload = payload
+
+
+    def groupPost(self):
+
+        #will post the payload to the group as its own post
+
+
+    def groupComment(self):
+
+        #will post the payload to the group as a comment to a group post
+
+
+    def subComment(self):
+
+        #will post the payload as a subcomment to a comment
+
---
+++ source/PP.py
@@ -0,0 +1,34 @@
+class PP:
+
+    def __init__(self, configFileText, memberNames):
+        self.configFileText = configFileText
+        self.memberNames = memberNames
+        self.payload = None
+
+
+
+    def __init__(self, configFileText):
+        self.configFileText = configFileText
+        self.payload = None
+
+
+
+    def STRpayload(self):
+
+    """This method will grab member names that have not posted their swing tags and the correct config file text. It will prepare a payload to be sent to the group poster
+    """
+
+    def MUpayload(self):
+
+    """This method will grab member names that have recently join the group as well the correct config file text for welcoming new members. It will prepare a payload to be sent to the group poster"""
+
+
+
+    def GNpayload(self):
+
+    """This method will grab the correct config file post. It will be used to prepare a payload to the group poster"""
+
+
+
+
+
